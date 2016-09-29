e0ea23cbd6649cd03abca84cbb1059302e19f55b
Xavier D. Johnson <me@xavierdjohnson.com>
2016-09-29 19:32:25 -0400

    inital commit of single view ios project

---
+++ oursoundboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oursoundboard
+//
+//  Created by Xavier D. Johnson on 9/29/16.
+//  Copyright © 2016 Xavier D. Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
